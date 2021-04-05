5454bb957c386f5a71447664803d8f8c527f8ab2
Leo <u583011@T0003379542.telecom.arg.telecom.com.ar>
2021-04-05 14:23:52 -0300

    Ahi volvi a subirlo comentando todo lo de DAO que no compilaba

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -15,37 +15,37 @@ public class Principal {
 		Gestion gestion = new Gestion();
 		Show show = new Show();
 		Evento evento = new Evento();
-		EventoDAO DAO = new EventoDAO();
+	//	EventoDAO DAO = new EventoDAO();
 		
 		Date fecha = new Date();
 	//	SimpleDateFormat fechaFormateada = new SimpleDateFormat("dd/MM/yyyy");
 	
-		System.out.println(DAO.toString());
+	//	System.out.println(DAO.toString());
 	//System.out.println(fechaFormateada.format(fecha));
 		
 //
-//	show.setIdShow(4);
-//evento.setDuracion(12);
-//evento.setFecha(fecha);
-////evento.setIdentrada();
-//evento.setIdEvento(1);
-//evento.setTipoEntrada("CUAL ES LA DIFERENCIA CON TIPO DE EVENTO??");
-//evento.setTipoEvento("Recital");
-//evento.setTitulo("LPDA EN LUNA PARK");
-//evento.setValor(50);
+	show.setIdShow(4);
+evento.setDuracion(12);
+evento.setFecha(fecha);
+//evento.setIdentrada();
+evento.setIdEvento(1);
+evento.setTipoEntrada("CUAL ES LA DIFERENCIA CON TIPO DE EVENTO??");
+evento.setTipoEvento("Recital");
+evento.setTitulo("LPDA EN LUNA PARK");
+evento.setValor(50);
 //
-//System.out.println("Datos del SHOW: " + show.toString());
-//
-//System.out.println("");
-//
-//System.out.println("Datos del EVENTO: " + evento.toString());
-//
-//System.out.println("");
-//
-//System.out.println("Datos de la ENTRADA: ");
-//System.out.println("Id de Entrada: "+ evento.getIdentrada());
-//System.out.println(("Tipo de entrada: "+ evento.getTipoEntrada()));
-//System.out.println(("Valor de entrada: "+ evento.getValor()));
+System.out.println("Datos del SHOW: " + show.toString());
+
+System.out.println("");
+
+System.out.println("Datos del EVENTO: " + evento.toString());
+
+System.out.println("");
+
+System.out.println("Datos de la ENTRADA: ");
+System.out.println("Id de Entrada: "+ evento.getIdentrada());
+System.out.println(("Tipo de entrada: "+ evento.getTipoEntrada()));
+System.out.println(("Valor de entrada: "+ evento.getValor()));
 
 
 
