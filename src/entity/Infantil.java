70e8d55087a1e0f659553032e7662ef5bef03b1b
u583011 <u583011@T0003379542.telecom.arg.telecom.com.ar>
2021-04-05 15:32:51 -0300

    Ultimos cambios con lo visto con crii

--- src/entity/Infantil.java
+++ src/entity/Infantil.java
@@ -7,17 +7,35 @@ public class Infantil extends Evento {
 	private boolean souvenir;
 	private int edad;
 	private static int cont = 0;
+	
+	//Modif Leo
+	private int precio;
+	private final int precioMayores = 250;
+	private final int  precioMenores = 500;
 
 	// CONSTRUCTORES
 
 	public Infantil() {
-		super();
+	super();
+	if (edad < 8) {
+		this.precio = precioMenores;
+	}
+	{
+		this.precio = precioMayores;
+	}
+
 	}
 
 	public Infantil(boolean souvenir, int edad) {
 		super();
 		this.souvenir = souvenir;
 		this.edad = edad;
+		if (edad < 8) {
+			this.precio = precioMenores;
+		}
+		{
+			this.precio = precioMayores;
+		}
 
 	}
 
@@ -46,5 +64,13 @@ public class Infantil extends Evento {
 	public static void setCont(int cont) {
 		Infantil.cont = cont;
 	}
+
+	@Override
+	public String toString() {
+		return "Infantil [souvenir=" + souvenir + ", edad=" + edad + ", precio=" + precio + ", precioMayores="
+				+ precioMayores + ", precioMenores=" + precioMenores + "]";
+	}
+	
+	
 
 }
--- src/main/Principal.java
+++ src/main/Principal.java
@@ -6,6 +6,7 @@ import dao.EventoDAO;
 import entity.Entrada;
 
 import entity.Evento;
+import entity.Infantil;
 import entity.Show;
 import util.Gestion;
 
@@ -24,6 +25,24 @@ public class Principal {
 	//System.out.println(fechaFormateada.format(fecha));
 		
 //
+		
+		
+		
+		
+//		Infantil prueba = new Infantil();
+	//	System.out.println(prueba.toString());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	show.setIdShow(4);
 evento.setDuracion(12);
 evento.setFecha(fecha);
@@ -33,15 +52,15 @@ evento.setTipoEntrada("CUAL ES LA DIFERENCIA CON TIPO DE EVENTO??");
 evento.setTipoEvento("Recital");
 evento.setTitulo("LPDA EN LUNA PARK");
 evento.setValor(50);
-//
+///
 System.out.println("Datos del SHOW: " + show.toString());
-
+//
 System.out.println("");
-
+//
 System.out.println("Datos del EVENTO: " + evento.toString());
-
+//
 System.out.println("");
-
+//
 System.out.println("Datos de la ENTRADA: ");
 System.out.println("Id de Entrada: "+ evento.getIdentrada());
 System.out.println(("Tipo de entrada: "+ evento.getTipoEntrada()));
