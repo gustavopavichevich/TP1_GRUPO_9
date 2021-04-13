23af4ebbb2a48ce49599eca9b728bf76d861ef3c
German Medina <gmedinasistemas@gmail.com>
2021-04-13 09:22:12 -0300

    Comienzo_TP2
    Realizo esta parte:
    Crear un paquete llamado resolución y dentro de él, realizar los siguientes puntos:
    a. Crear un main llamado ejercicio1. Dentro de este main crear 5 entradas y guardarlas dentro de un ArrayList. Mostrar las entradas guardadas en el ArrayList con un iterador.

---
+++ src/resolucion/ejercicio1.java
@@ -0,0 +1,33 @@
+package resolucion;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		entradas p1 = new entradas("Belen", "Sanchez", "11111111","arenales","femenino","san miguel","buenos aires");
+		entradas p2 = new entradas("Alberto", "Rodriguez", "22222222","arenales","femenino","san miguel","buenos aires");
+		entradas p3 = new entradas("Rodrigo", "Fernandez", "33333333","arenales","femenino","san miguel","buenos aires");
+		entradas p4 = new entradas("Pedro", "Iba�ez", "444444444","arenales","femenino","san miguel","buenos aires");
+		entradas p5= new entradas("Mariano", "Martinez", "555555","arenales","femenino","san miguel","buenos aires");
+		
+		ArrayList<entradas> listaEntradas = new ArrayList<entradas>();
+		listaEntradas.add(p1);
+		listaEntradas.add(p1);
+		listaEntradas.add(p2);
+		listaEntradas.add(p3);
+		listaEntradas.add(p4);
+		listaEntradas.add(p5);
+		
+		System.out.println("Listado de entradas \n");		
+		Iterator<entradas> it = listaEntradas.iterator();
+		while(it.hasNext())
+		{
+			entradas p = (entradas) it.next();
+			System.out.println(p.toString());
+		}
+	}
+
+}
---
+++ src/resolucion/entradas.java
@@ -0,0 +1,134 @@
+package resolucion;
+
+public class entradas {
+	private String nombre;
+	private String apellido;
+	private String dni;
+	private String direccion;
+	private String genero;
+	private String localidad;
+	private String provincia;
+	
+	
+	
+	public entradas(String nombre, String apellido, String dni, String direccion, String genero, String localidad,
+			String provincia) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.direccion = direccion;
+		this.genero = genero;
+		this.localidad = localidad;
+		this.provincia = provincia;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	@Override
+	public String toString() {
+		return "entradas [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", direccion=" + direccion
+				+ ", genero=" + genero + ", localidad=" + localidad + ", provincia=" + provincia + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((direccion == null) ? 0 : direccion.hashCode());
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+		result = prime * result + ((localidad == null) ? 0 : localidad.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((provincia == null) ? 0 : provincia.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		entradas other = (entradas) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (direccion == null) {
+			if (other.direccion != null)
+				return false;
+		} else if (!direccion.equals(other.direccion))
+			return false;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		if (genero == null) {
+			if (other.genero != null)
+				return false;
+		} else if (!genero.equals(other.genero))
+			return false;
+		if (localidad == null) {
+			if (other.localidad != null)
+				return false;
+		} else if (!localidad.equals(other.localidad))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (provincia == null) {
+			if (other.provincia != null)
+				return false;
+		} else if (!provincia.equals(other.provincia))
+			return false;
+		return true;
+	}
+
+
+
+}
